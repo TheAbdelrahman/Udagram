7818f59582a2b054b8830329c081180a0a294902
Abdelrahman Abdullah <107592121+TheAbdelrahman@users.noreply.github.com>
2022-12-08 11:04:57 +0200

    solving first review issues: FE to api host

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'udagram',
   apiHost: 'http://udagram-env.eba-znuh8gun.us-east-1.elasticbeanstalk.com/api/v0'
 };
